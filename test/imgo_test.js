377eb1aebec21f572f6f29a8be117398fcb3d5ee
Artem Sapegin <artem@sapegin.ru>
2012-11-06 16:41:48 +0400

    Tests.

---
+++ test/imgo_test.js
@@ -0,0 +1,21 @@
+var fs = require('fs'),
+	grunt = require('grunt');
+
+exports.imgo = {
+	compile: function(test) {
+		'use strict';
+
+		var files = grunt.file.expand('test/src/**');
+
+		test.expect(files.length);
+
+		files.forEach(function(srcFile) {
+			var dstFile = srcFile.replace('/src/', '/tmp/'),
+				srcSize = fs.statSync(srcFile).size,
+				dstSize = fs.statSync(dstFile).size;
+			test.ok(dstSize < srcSize, 'Resulting file should be less than source.');
+		});
+
+		test.done();
+	}
+};
